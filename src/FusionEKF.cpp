2a24fcef240c2b70c934ac2a7a3faafaa9679f4e
Amrose Birani <amrose.birani@indiez.io>
2018-05-13 22:20:57 +0530

    adding all the changes required for project

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -31,12 +31,32 @@ FusionEKF::FusionEKF() {
         0, 0.0009, 0,
         0, 0, 0.09;
 
+  H_laser_ << 1, 0, 0, 0,
+			  0, 1, 0, 0;
   /**
   TODO:
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
+  MatrixXd P_ = MatrixXd(4, 4);
 
+  P_ << 1, 0, 0, 0,
+			  0, 1, 0, 0,
+			  0, 0, 1000, 0,
+			  0, 0, 0, 1000;
+  
+  MatrixXd F_ = MatrixXd(4,4);
+
+  F_ << 1, 0, 1, 0,
+			  0, 1, 0, 1,
+			  0, 0, 1, 0,
+			  0, 0, 0, 1;
+  
+  VectorXd x_ = VectorXd(4);
+
+  MatrixXd Q_ = MatrixXd(4, 4);
+  
+  ekf_.Init(x_, P_, F_, H_laser_, R_laser_, Q_);
 
 }
 
@@ -67,14 +87,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+      double x = measurement_pack.raw_measurements_[0]*cos(measurement_pack.raw_measurements_[1]);
+      double y = measurement_pack.raw_measurements_[0]*sin(measurement_pack.raw_measurements_[1]);
+      ekf_.x_ << x, y, 0, 0;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
+     ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
     }
 
     // done initializing, no need to predict or update
+    previous_timestamp_ = measurement_pack.timestamp_;
     is_initialized_ = true;
     return;
   }
@@ -91,6 +116,23 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
 
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+	previous_timestamp_ = measurement_pack.timestamp_;
+
+  ekf_.F_ << 1, 0, dt, 0,
+			  0, 1, 0, dt,
+			  0, 0, 1, 0,
+			  0, 0, 0, 1;
+  
+  float t4 = pow(dt, 4)/4;
+  float t3 = pow(dt, 3)/2;
+  float t2 = pow(dt, 2);
+
+  ekf_.Q_ << t4*9, 0, t3*9, 0,
+			  0, t4*9, 0, t3*9,
+			  t3*9, 0, t2*9, 0,
+			  0, t3*9, 0, t2*9;
+
   ekf_.Predict();
 
   /*****************************************************************************
@@ -105,8 +147,18 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    ekf_.R_ = R_radar_;
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+    VectorXd zz(3);
+    zz << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], measurement_pack.raw_measurements_[2];
+    ekf_.UpdateEKF(zz);
   } else {
     // Laser updates
+    ekf_.R_ = R_laser_;
+    ekf_.H_ = H_laser_;
+    VectorXd z(2);
+    z << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1];
+    ekf_.Update(z);
   }
 
   // print the output
--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,28 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+  if (estimations.size() == 0 || estimations.size() != ground_truth.size()) {
+	    cout << "Bad Input" << endl;
+	    return rmse;
+	}
+
+  for(int i=0; i < estimations.size(); ++i){
+    VectorXd diff = estimations[i] - ground_truth[i];
+    VectorXd sqd = diff.array()*diff.array();
+		rmse[0] = rmse[0] + sqd[0];
+		rmse[1] = rmse[1] + sqd[1];
+		rmse[2] = rmse[2] + sqd[2];
+		rmse[3] = rmse[3] + sqd[3];
+	}
+
+  rmse = rmse/estimations.size();
+
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +44,24 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+  MatrixXd Hj(3,4);
+  float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+  float c1 = px*px+py*py;
+	float c2 = sqrt(c1);
+	float c3 = (c1*c2);
+
+  if(fabs(c1) < 0.0001){
+		cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+		return Hj;
+	}
+
+  Hj << (px/c2), (py/c2), 0, 0,
+		  -(py/c1), (px/c1), 0, 0,
+		  py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+
+	return Hj;
 }
